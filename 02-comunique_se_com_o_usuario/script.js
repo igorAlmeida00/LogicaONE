964e9380072798639ce196f37d4f78a5ae9b51c2
igorAlmeida00 <igor77alme@outlook.com>
2022-07-29 01:05:24 -0300

    02

---
+++ 02-comunique_se_com_o_usuario/script.js
@@ -0,0 +1,6 @@
+document.write("A idade de fulano e:");
+document.write(18 + 20);
+
+document.write("A media de fulano e:" + (9 + 10 + 7)/ 3);
+console.log("Soma", 10 + 10);
+document.write(`Fulano nasceu em ${2022 - 24}`);
