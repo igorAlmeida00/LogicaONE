de56c896619f6c4eace8c942c4dd20d33508be1c
igorAlmeida00 <igor77alme@outlook.com>
2022-07-29 02:22:07 -0300

    add 04

--- 03-programador_dinamico_com_variaveis/script.js
+++ 03-programador_dinamico_com_variaveis/script.js
@@ -7,3 +7,24 @@ let nascimento = 1998;
 let idade = ano - nascimento;
 
 console.log(`Funalo tem ${idade} anos`);
+
+//media
+
+let enzo = 17;
+let ze = 37;
+let gio = 25
+
+let media = (enzo + ze + gio) / 3;
+
+console.log(`A media entre as idades e: ${Math.round(media)} anos`);
+
+/*Álcool ou Gasolina? */
+
+let tanque = 40;
+let caminhoComGasolina = 480;
+let caminhoComAlcool = 300;
+let consumoDeGasolina = caminhoComGasolina/tanque;
+let consumoDeAlcool = caminhoComAlcool/tanque;
+
+console.log(`Seu consumo de gasolina foi de: ${consumoDeGasolina}Km/L`);
+console.log(`Seu consumo alcool foi de: ${consumoDeAlcool}Km/L`);
---
+++ 04-crie-suas-proprias-funcionalidades/script.js
@@ -0,0 +1,3 @@
+function pularLinha(){
+  document.write("<br><br>");
+}
