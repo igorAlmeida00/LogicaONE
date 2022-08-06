a43bf310c90b1074a073c03710379eeda22a6977
igorAlmeida00 <igor77alme@outlook.com>
2022-08-06 16:59:08 -0300

    atualizacao

--- 09-trabalhe-com-muitos-dados/script.js
+++ 09-trabalhe-com-muitos-dados/script.js
@@ -1,17 +1,73 @@
-let secret = Math.round(Math.random() * 10);
-
 let input = document.querySelector("input");
-let btn = document.querySelector("button");
 input.focus();
+let button = document.querySelector("button");
+button.onclick = verifica;
 
-function verificar() {
-  if(input.value == secret) {
-    alert("vc acertou"); 
-  } else {
-    alert("vc errou");
+function sorteia() {
+  return Math.round(Math.random() * 10);
+}
+
+function sorteiaNumeros(quantidade) {
+  let segredos = [];
+  let numero = 1;
+  while(numero <= quantidade) {
+    let numeroAleatorio = sorteia();
+    let achou = false;
+    if (numeroAleatorio !== 0) {
+      for(var posicao = 0; posicao < segredos.length; posicao++) {
+        if(segredos[posicao] == numeroAleatorio){
+          achou = true;
+          break;
+        }
+      }
+      if (achou == false) {
+        segredos.push(numeroAleatorio);
+        numero++;
+      }
+    }
+  }
+  return segredos;
+}
+
+let segredos = sorteiaNumeros(3);
+console.log(segredos);
+
+function verifica() {
+  var achou = false;
+  for(var posicao = 0; posicao < segredos.length; posicao++) {
+    if(input.value == segredos[posicao]) {
+      alert("Você ACERTOU!");
+      achou = true;
+      break;
+    } 
+  }
+
+  if(achou == false) {
+    alert("Você ERROU!");
   }
   input.value = "";
   input.focus();
 }
 
-btn.onclick = verificar;
+// === === Não existe ingrediente repetido aqui! === === //
+/*let ingredientes = [];
+let quantidade = Number(prompt("Quantos ingredientes você vai adicionar?"));
+let contador = 1;
+
+while(contador <= quantidade) {
+  let ingrediente = prompt("Informe o ingrediente " + contador);
+  let existe = false;
+  for(posicao = 0; posicao < ingredientes.length; posicao++) {
+    if(ingredientes[posicao] == ingrediente) {
+      existe = true;
+      break;
+    }
+  }
+
+  if(existe == false) {
+    ingredientes.push(ingrediente);
+    contador++;
+  }
+}
+
+console.log(ingredientes);*/
